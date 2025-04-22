6843f79df5d211cec968a4254ef5c1264ce32265
duber <besanson@hotmail.com>
2025-04-22 11:56:41 +0200

    Update products.controller.test.ts getAll method

--- src/back/products.controller.test.ts
+++ src/back/products.controller.test.ts
@@ -1,5 +1,5 @@
 import { ProductsController } from './products.controller';
-import { vi } from 'vitest';
+import { vi, Mock } from 'vitest';
 
 const mockRepo = {
     read: vi.fn().mockResolvedValueOnce([]),
@@ -9,6 +9,20 @@ const mockRepo = {
     delete: vi.fn().mockResolvedValueOnce([]),
 };
 
+const req = {
+    params: {},
+    body: {},
+} as unknown as Request;
+
+const res = {
+    json: vi.fn(),
+    status: vi.fn(),
+} as unknown as Response;
+
+const next = vi.fn() as NextFunction;
+
+const error = new Error('Error');
+
 describe('roductsController', () => {
     const productsRepo = new ProductsController(mockRepo);
 
@@ -16,5 +30,25 @@ describe('roductsController', () => {
         //Assert
         expect(productsRepo).toBeDefined();
         expect(productsRepo).toBeInstanceOf(ProductsController);
+    });
+
+    describe('getAll method should be call', () => {
+        test('Json should be called json when response is valid', async () => {
+            //Act
+            await productsRepo.getAll(req, res, next);
+            //Assert
+            expect(res.json).toHaveBeenCalledWith({
+                results: [],
+                error: '',
+            });
+        });
+        test('Next should be called when throw an error', async () => {
+            //Arrange
+            (mockRepo.read as Mock).mockRejectedValueOnce(error);
+            //Act
+            await productsRepo.getAll(req, res, next);
+            //Assert
+            expect(next).toHaveBeenCalledWith(error);
+        });
     });
 });
