e3220d068086fdc9795cf23b5a4fed2f33890b1d
duber <besanson@hotmail.com>
2025-04-22 13:11:10 +0200

    Update form.add.test.ts handleSubmit fn

--- src/front/components/form.add.test.ts
+++ src/front/components/form.add.test.ts
@@ -1,10 +1,21 @@
 import { createFormAdd } from './form.add';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/dom';
 
-describe('Give createFormAdd', () => {
-    test('When is create the form', () => {
+describe('Given createFormAdd', () => {
+    test('From is created', () => {
         //Act
         const form = createFormAdd();
         //Assert
         expect(form).toBeInstanceOf(HTMLFormElement);
+    });
+
+    test('Submit button is clicked', () => {
+        //Act
+        const button = screen.getByRole('button', { name: 'Crear' });
+        button.click();
+        //Assert
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveAttribute('type', 'submit');
     });
 });
